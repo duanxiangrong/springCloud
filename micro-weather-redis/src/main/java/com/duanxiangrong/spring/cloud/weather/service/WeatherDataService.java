7c588c4dd6f72f31c78564475fb7986a5e8eca18
duanxiangrong <duanxiangrong@gmail.com>
2018-10-04 19:03:18 +0800

    spring boot redis

---
+++ micro-weather-redis/src/main/java/com/duanxiangrong/spring/cloud/weather/service/WeatherDataService.java
@@ -0,0 +1,26 @@
+package com.duanxiangrong.spring.cloud.weather.service;
+
+import com.duanxiangrong.spring.cloud.weather.vo.WeatherResponse;
+
+/*
+ * 天气数据服务
+ */
+public interface WeatherDataService {
+	
+	/*
+	 * 根据城市ID来查询天气数据
+	 * 
+	 * @param cityId
+	 * @return
+	 */
+	WeatherResponse getDataByCityId(String cityId );
+	
+	/*
+	 * 根据城市名称来查询天气数据
+	 * 
+	 * @param cityName
+	 * @return 
+	 */
+	WeatherResponse getDataByCityName(String cityName);
+
+}
